05c6f51ccdfaa6f896472434465653865a2bd1e9
Fu <fu.hoang@ntoklo.com>
2015-11-11 15:25:29 +0000

    crop img on sidebar

--- single.php
+++ single.php
@@ -21,16 +21,51 @@
 	  </div>
 	  <div class = "col-md-4 col-xs-12">
 	  	<div class="sidebar-container">
+	  		<?php
+	  		$post_id = get_the_ID();
+				$tags = wp_get_post_tags($post_id);
+				if ($tags) {
+				$first_tag = $tags[0]->term_id;
+				$args=array(
+				'tag__in' => array($first_tag),
+				'post__not_in' => array($post_id),
+				'posts_per_page'=>5,
+				'caller_get_posts'=>1
+				);
+				$my_query = new WP_Query($args);
+				if( $my_query->have_posts() ) {
+				?>
+				<h2>Related Posts</h2>
+				<ul>
+				<?php
+				while ($my_query->have_posts()) : $my_query->the_post(); ?>
+					<li>
+						<span class="sidebar-img-container">
+							<a href="<?php the_permalink();?>"><?php the_post_thumbnail('recent-thumbnails', array( 'class'  => "img-responsive")); ?></a>
+							<h4><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
+						</span>
+					</li>
+				<?php
+				endwhile;
+				?>
+				</ul>
+				<?php
+				}
+				wp_reset_query();
+				}
+	  		?>
+
 		  	<h2>Recent Post</h2>
-				<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
 				<ul>
 				<?php
 				$rPosts = new WP_Query();
 				$rPosts->query('showposts=3');
 				while ($rPosts->have_posts()) : $rPosts->the_post(); ?>
 					<li>
-						<a href="<?php the_permalink();?>"><?php the_post_thumbnail('recent-thumbnails', array( 'class'  => "img-responsive")); ?></a>
-						<h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+						<span class="sidebar-img-container">
+							<a href="<?php the_permalink();?>"><?php the_post_thumbnail('recent-thumbnails', array( 'class'  => "img-responsive")); ?></a>
+							<h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+						</span>
 					</li>
 				<?php endwhile;
 				wp_reset_query();?>
